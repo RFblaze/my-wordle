67dfbb049290dee3edd410cf5f165affff127b94
Angelo Troncone <angelotroncone@gmail.com>
2023-02-15 23:10:52 -0700

    Added Victory gif, Darkmode works

--- wordle/wordle.js
+++ wordle/wordle.js
@@ -2,33 +2,66 @@
 // it's just here to shows it works
 // window.alert("works!");
 
-
-
-const getWord = (dictionary) =>{
+const getWordAndHint = (dictionary) =>{
   const N = Object.keys(dictionary).length
   const randomNum = Number.parseInt(Math.random() * N)
-  
-  console.log(randomNum)
+  const wordAndHint = dictionary[randomNum]
+  console.log(wordAndHint)
+  const {word} = wordAndHint
+  const {hint} = wordAndHint
+  currentWord = word
+  currentHint = hint
 }
 
-
+var currentWord
+var currentHint
+var viewTheme = true
 
 const getDict = async() =>{
   const res = await fetch("https://api.masoudkf.com/v1/wordle", {headers: {"x-api-key": "sw0Tr2othT1AyTQtNDUE06LqMckbTiKWaVYhuirv",},})
   const json = await res.json();
   const {dictionary} = json 
-  console.log(dictionary)
-  getWord(dictionary)
+  getWordAndHint(dictionary)
 }
 
-
-
-  const darkMode = () =>{
-    let body = document.body
-    if (body.getPropertyValue("background-color") === "#FFFFFF"){
-      document.body.style.backgroundColor = "#252525";
-      document.body.style.color = "#FFFFFF";
-    }
+const darkMode = () =>{
+  let mainBody = document.body
+  if (viewTheme){
+    let navbar = document.querySelector("#topBar")
+    navbar.style.borderBottom = "solid 1px #CACACA"
+    mainBody.style.backgroundColor = "#252525"
+    mainBody.style.color = "#FFFFFF"
+    viewTheme = false
   }
+
+  else{
+    mainBody.style.backgroundColor = "#FFFFFF"
+    mainBody.style.color = "#000000"
+    viewTheme = true
+  }
+}
+
+let letterList = []
+
+const checkAnswer = () =>{
+
+}
+
+const fillTable = () =>{
+
+}
+
+function takeInput(){
+  let letter = document.getElementById("input").value
+  if (letter.match(/[a-zA-z]/) != null && letter.length == 1){
+    console.log(letter)
+  }
+
+  else if (letter.toLowerCase() == "backspace"){
+    
+  }
+  
+}
+
 
 
